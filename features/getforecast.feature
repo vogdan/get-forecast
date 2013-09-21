3a47f30abbf9b9313714a51228dd5026929290ca
“Bogdan” <dbogdan@gmail.com>
2013-09-21 18:29:52 +0300

    Added geolocation transform (only works for US)

--- features/getforecast.feature
+++ features/getforecast.feature
@@ -1,6 +1,11 @@
 Feature: Get Forecast
 
-         Scenario: Get forecast for Moreni
+         Scenario: Get forecast for Moreni decimal coordinates
                    Given the "44.9802778,25.6444444" latitude and longitude coordinates
                    Then get the forecast                   
                    And save forecast if it's rainy
+                   
+         Scenario: Get forecast for Moreni string address
+                   Given the "100 Spear St, San Francisco, CA" address
+                   Then get the forecast
+                   And save forecast if it's rainy       
--- features/step_definitions/forecast_steps.rb
+++ features/step_definitions/forecast_steps.rb
@@ -1,5 +1,8 @@
-require "rubygems"
+require 'rubygems'
 require 'forecast_io'
+require 'geokit'
+
+include GeoKit::Geocoders
 
 ForecastIO.configure do |configuration|
   configuration.api_key = 'e26ea8c99939d15c0e037b208ca06df3'
@@ -9,9 +12,20 @@ GET_CRD = Transform /^(-?[0-9\.]+)$/ do |coordinate|
   coordinate.to_i
 end
 
-  Given /^the "(#{GET_CRD}),(#{GET_CRD})" latitude and longitude coordinates$/ do |latitude, longitude|
+GET_CRDS_FROM_ADDRESS = Transform /^([0-9a-zA-Z, ]+)$/ do |address|
+  coordinates = MultiGeocoder.geocode(address)
+end
+
+Given /^the "(#{GET_CRD}),(#{GET_CRD})" latitude and longitude coordinates$/ do |latitude, longitude|
   @latitude = latitude
   @longitude = longitude
+end
+
+Given /^the "(#{GET_CRDS_FROM_ADDRESS})" address$/ do |coordinates|
+  @latitude = coordinates.lat
+  @longitude = coordinates.lng
+#  puts "latitude:" + @latitude
+#  puts "longitude" + @longitude
 end
 
 Then /^get the forecast$/ do
@@ -26,4 +40,7 @@ And /^save forecast if it\'s rainy$/ do
     puts "NO RAIN, MAN!"
   end
 end
+
+
+
 
